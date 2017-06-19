9dcaf9e238822e3dd36900eae31bd971576fa8bd
Brendan McCoy <bmccoy@live.com>
2017-06-18 17:03:35 -0700

    Replace Xiaomi build fingerprint with Nextbit fingerprint

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -66,8 +66,8 @@ elif [ -f "/data/magisk/magisk" ]; then RESETPROP="/data/magisk/magisk $RESETPRO
 elif [ -f "/magisk/.core/bin/resetprop" ]; then RESETPROP="/magisk/.core/bin/$RESETPROP"
 elif [ -f "/data/magisk/resetprop" ]; then RESETPROP="/data/magisk/$RESETPROP"; fi
 
-$RESETPROP "ro.build.fingerprint" "Xiaomi/sagit/sagit:7.1.1/NMF26X/V8.2.17.0.NCACNEC:user/release-keys"
-$RESETPROP "ro.bootimage.build.fingerprint" "Xiaomi/sagit/sagit:7.1.1/NMF26X/V8.2.17.0.NCACNEC:user/release-keys"
+$RESETPROP "ro.build.fingerprint" "Nextbit/ether/ether:7.1.1/Robin_Nougat_108/00WW_Jenkins_108:user/release-keys"
+$RESETPROP "ro.bootimage.build.fingerprint" "Nextbit/ether/ether:7.1.1/Robin_Nougat_108/00WW_Jenkins_108:user/release-keys"
 $RESETPROP "ro.build.type" "user"
 $RESETPROP "ro.build.tags" "release-keys"
 $RESETPROP "ro.build.selinux" "0"
